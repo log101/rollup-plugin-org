e1c568912d233bb973333700703f0676225826b1
log101 <ffrknerdm@gmail.com>
2024-02-02 10:57:17 +0300

    init project

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import { createFilter } from '@rollup/pluginutils'
+
+export default function org(options = {}) {
+    const filter = createFilter(options.include || ['**/*.org'], options.exclude)
+
+    return {
+        name: 'org',
+        transform(org, id) {
+            if (!filter(id)) return null
+
+            const data = org
+            return {
+                code: `export default ${JSON.stringify(data.toString())};`,
+                map: { mappings: '' }
+            }
+
+        }
+
+    }
+}
